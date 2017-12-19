59dfae8e0a0bf4180e11cee92b10876bf0a5b382
Chia T <tham_oh@hotmail.com>
2017-12-19 00:37:32 -0600

    fixed js

--- contactCard.js
+++ contactCard.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
 
     $('form').submit(function() {
         console.log($(this).serialize());
-        $('#rightside').append('<div id="card"><p>Move your mouse on the name</p
-        ><p id="cardFront">' 
+        $('#rightside').append('<div id="card"><p>Move your mouse on the name</p><p id="cardFront">' 
             + $('input[name="firstname"]').val() + ' '
             + $('input[name="lastname"]').val() 
             + '<p id="cardBack">'
